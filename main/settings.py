f9b0c65a6a55a1fe29ba9ede84e48431f05fd6cd
guneysrhat <guneysrhat@gmail.com>
2023-05-13 12:42:51 +0300

    serializers

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Modules:
+    'rest_framework',
+    
+    # Apps:
+    'todo'
 ]
 
 MIDDLEWARE = [
